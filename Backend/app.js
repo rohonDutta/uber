cbc933f64a7389df176c9198ff3801ae58a92572
rohonDutta <rohonkumardutta@gmail.com>
2025-07-02 17:56:15 +0530

    user authentication done

--- Backend/app.js
+++ Backend/app.js
@@ -3,22 +3,21 @@ dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const cookiesParser = require("cookie-parser");
 const connectToDb = require("./db/db");
-const userRoutes = require('./routes/user.routes');
-connectToDb();
+const userRoutes = require("./routes/user.routes");
 
+connectToDb();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-
-
+app.use(cookiesParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use('/users', userRoutes);
+app.use("/users", userRoutes);
 
 module.exports = app;
--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -2,6 +2,7 @@ const userModel = require("../models/user.model");
 const userService = require("../services/user.service");
 const { validationResult } = require("express-validator");
 
+const blacklistTokenModel = require("../models/blackListToken.model");
 module.exports.registerUser = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
@@ -32,7 +33,7 @@ module.exports.registerUser = async (req, res, next) => {
   const token = user.generateAuthToken();
 
   res.status(201).json({ token, user });
-};
+}
 
 module.exports.loginUser = async (req, res, next) => {
   const errors = validationResult(req);
@@ -55,6 +56,21 @@ module.exports.loginUser = async (req, res, next) => {
   }
 
   const token = user.generateAuthToken();
+res.cookie('token', token);
+
 
   res.status(200).json({ token, user });
-};
+}
+
+module.exports.getUserProfile = async (req, res, next) => {
+  res.status(200).json(req.user);
+}
+
+module.exports.logoutUser = async (req, res, next) => {
+  res.clearCookie('token');
+  const token = req.cookies.token || req.headers.authorization?.split(' ')[1];
+
+await blacklistTokenModel.create({ token });
+
+  res.status(200).json({ message: "Logged out successfully" });
+}
